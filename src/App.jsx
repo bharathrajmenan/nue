b7efcab587f6956e735cd0e19459a04e82b528ec
bharathrajmenan <brm@example.com>
2025-04-07 12:28:10 +0530

    bharath raj menan

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <div class="center">
+    <h1>
+     PEACE
+    </h1>
+     </div>
+    </div>
+  )
+}
+
+export default App
